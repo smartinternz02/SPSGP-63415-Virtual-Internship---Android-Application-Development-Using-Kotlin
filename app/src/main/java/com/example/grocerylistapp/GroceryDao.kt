9e3a7e6f56cd7175164f85d9884ccc2eebf6ac55
Ishan <ishank20062002@gmail.com>
2022-09-26 13:28:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/grocerylistapp/GroceryDao.kt
@@ -0,0 +1,18 @@
+package com.example.grocerylistapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface GroceryDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(item: GroceryItems)
+
+    @Delete
+    fun delete(item: GroceryItems)
+
+    @Query("SELECT * FROM grocery_items")
+    fun getAllGroceryItems(): LiveData<List<GroceryItems>>
+
+}
